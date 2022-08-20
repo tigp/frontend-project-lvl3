9af2e3e5ca247ba7591fbf048882851464c1b25f
tigp <hnztkfc@gmail.com>
2022-08-20 17:26:41 +0300

    changed webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -8,12 +8,20 @@ const config = {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js',
     },
+    devServer: {
+        open: true,
+        host: 'localhost',
+    },
     plugins: [new HtmlWebpackPlugin({ template: './index.html' })],
     module: {
         rules: [
             {
                 test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
                 type: 'asset',
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
             },
         ],
     },
