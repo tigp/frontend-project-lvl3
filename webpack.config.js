911cb34be131d272ffe3f3724a9eccc4b82759eb
tigp <hnztkfc@gmail.com>
2022-09-12 14:34:46 +0300

    fixed indent

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
   devServer: {
     open: true,
     host: 'localhost',
-    },
+  },
   plugins: [new HtmlWebpackPlugin({ template: './index.html' })],
   module: {
     rules: [
@@ -20,8 +20,8 @@ module.exports = {
         type: 'asset',
       },
       {
-      test: /\.css$/,
-      use: ['style-loader', 'css-loader'],
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
       },
     ],
   },
