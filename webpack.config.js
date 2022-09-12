c2dd32fc8dd21b139e5f37824ab8a0d978851f06
tigp <hnztkfc@gmail.com>
2022-09-12 14:27:28 +0300

    fixed indent

--- webpack.config.js
+++ webpack.config.js
@@ -2,27 +2,27 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    mode: 'development',
-    entry: './src/index.js',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'main.js',
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js',
+  },
+  devServer: {
+    open: true,
+    host: 'localhost',
     },
-    devServer: {
-        open: true,
-        host: 'localhost',
-    },
-    plugins: [new HtmlWebpackPlugin({ template: './index.html' })],
-    module: {
-        rules: [
-            {
-                test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
-                type: 'asset',
-            },
-            {
-                test: /\.css$/,
-                use: ['style-loader', 'css-loader'],
-            },
-        ],
-    },
+  plugins: [new HtmlWebpackPlugin({ template: './index.html' })],
+  module: {
+    rules: [
+      {
+        test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
+        type: 'asset',
+      },
+      {
+      test: /\.css$/,
+      use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
 };
