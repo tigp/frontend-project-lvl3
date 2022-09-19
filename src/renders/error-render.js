e6aad09d6e4bbe790fdbf4adefc9faf6e5598063
tigp <hnztkfc@gmail.com>
2022-09-19 19:14:51 +0300

    relocated renders in view and deleted

--- src/renders/error-render.js
+++
@@ -1,7 +0,0 @@
-export default (error, elements, i18nInstance) => {
-  if (error === null) {
-    return;
-  }
-
-  elements.feedback.textContent = i18nInstance.t(`errors.${error}`);
-};
--- src/renders/modal-render.js
+++
@@ -1,9 +0,0 @@
-export default (watchedState, elements) => {
-  const targetPost = watchedState.posts
-    .flat()
-    .find((post) => post.id === watchedState.uiState.targetPostId);
-
-  elements.modalTitle.textContent = targetPost.title;
-  elements.modalBody.textContent = targetPost.description;
-  elements.modalLink.setAttribute('href', targetPost.link);
-};
